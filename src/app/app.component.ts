e7200ee2b662f3fa1cbbe5a21e64729cb883559b
nabil musa <nbl_musa@yahoo.com>
2018-10-24 00:13:59 +0400

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    @ViewChild('editor') editor;
+    isLoading = true;
+    direction = 'horizontal';
+    size = {
+        editor: 50,
+        viewer: 50
+    };
+    editorOptions = {
+        value: '// First line\nfunction hello() {\n\talert(\'Hello world!\');\n}\n// Last line',
+        language: 'html',
+        theme: 'vs-dark',
+        renderMinimap: false,
+        automaticLayout: true,
+        scrollbar: {
+            vertical: 'hidden'
+        },
+        minimap: {
+            enabled: false
+        }
+    };
+    code = `<html>
+	<body>
+		Hello world
+	</body>
+</html>`;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.isLoading = false;
+            this.run();
+        }, 2000);
+    }
+
+    changeOrientation() {
+        this.direction = this.direction === 'horizontal' ? 'vertical' : 'horizontal';
+    }
+
+    toggleView(view: number) {
+        switch (view) {
+            case 1:
+                this.size.editor = 100;
+                this.size.viewer = 0;
+                break;
+            case 2:
+                this.size.editor = 50;
+                this.size.viewer = 50;
+                break;
+            case 3:
+                this.size.viewer = 100;
+                this.size.editor = 0;
+                break;
+
+        }
+    }
+
+    run() {
+        const viewer: any = document.getElementById('viewer');
+        viewer.contentWindow.document.open();
+        viewer.contentWindow.document.write(this.code);
+        viewer.contentWindow.document.close();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularSplitModule} from 'angular-split';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularSplitModule,
+        FormsModule,
+        MonacoEditorModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
